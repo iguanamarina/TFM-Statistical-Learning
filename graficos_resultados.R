a1fa740b37a0b670e5893de8325bef5a802a371f
Juan A. Arias <34753691+iguanamarina@users.noreply.github.com>
2023-06-04 22:32:07 +0100

    Update graficos_resultados.R

--- graficos_resultados.R
+++ graficos_resultados.R
@@ -2,9 +2,10 @@ library(ggplot2)
 library(stringr)
 
 # Crear un dataframe con los datos
+
 data <- data.frame(
-  Modelo = factor(c("Árboles de Decisión", "Bosques Aleatorios", "SVM", "Aprendizaje Profundo", "Aprendizaje Profundo & FDA"), 
-                  levels = c("Árboles de Decisión", "Bosques Aleatorios", "SVM", "Aprendizaje Profundo", "Aprendizaje Profundo & FDA")),
+  Modelo = factor(c("AD", "BA", "SVM", "AP", "AP & FDA"), 
+                  levels = c("AD", "BA", "SVM", "AP", "AP & FDA")),
   Sensibilidad = c(0.2, 0.6, 0, 0.6667, 0.8),
   Especificidad = c(0.8571, 0.7142, 1, 0.8333, 0.86),
   Precisión = c(0.5833, 0.6666, 0.5833, 0.75, 0.8),
@@ -17,6 +18,7 @@ data_long <- reshape2::melt(data, id.vars = "Modelo")
 
 # Convertir la variable 'variable' a un factor y especificar el orden de los niveles
 data_long$variable <- factor(data_long$variable, levels = c("Sensibilidad", "Especificidad", "Precisión", "P_Balanceada", "Kappa_Coef"))
+
 
 # Crear el gráfico
 ggplot(data_long, aes(x = Modelo, y = value, fill = variable)) +
@@ -42,40 +44,50 @@ g1 <- ggplot(subset(data_long, variable == "Sensibilidad"), aes(x = Modelo, y = 
   geom_bar(stat = "identity", position = "dodge") +
   scale_fill_brewer(palette = "Blues") +
   theme_minimal() +
-  labs(x = "", y = "", title = "Sensibilidad") +
-  theme(axis.text.x = element_blank(), legend.position = "none", plot.title = element_text(hjust = 0.5, size = 20)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 18),
+        legend.position = "none",
+        plot.title = element_text(hjust = 0.5, size = 20)) +
+  labs(title = "Sensibilidad", x = "", y = "") +
   ylim(0, 1)
 
 g2 <- ggplot(subset(data_long, variable == "Especificidad"), aes(x = Modelo, y = value, fill = Modelo)) +
   geom_bar(stat = "identity", position = "dodge") +
   scale_fill_brewer(palette = "Greens") +
   theme_minimal() +
-  labs(x = "", y = "", title = "Especificidad") +
-  theme(axis.text.x = element_blank(), legend.position = "none", axis.title.y = element_blank(), plot.title = element_text(hjust = 0.5, size = 20), axis.text.y = element_blank()) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 18),
+        legend.position = "none",
+        plot.title = element_text(hjust = 0.5, size = 20)) +
+  labs(title = "Especificidad", x = "", y = "") +
   ylim(0, 1)
 
 g3 <- ggplot(subset(data_long, variable == "Precisión"), aes(x = Modelo, y = value, fill = Modelo)) +
   geom_bar(stat = "identity", position = "dodge") +
   scale_fill_brewer(palette = "Oranges") +
   theme_minimal() +
-  labs(x = "", y = "", title = "Precisión") +
-  theme(axis.text.x = element_blank(), legend.position = "none", axis.title.y = element_blank(), plot.title = element_text(hjust = 0.5, size = 20), axis.text.y = element_blank()) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 18),
+        legend.position = "none",
+        plot.title = element_text(hjust = 0.5, size = 20)) +
+  labs(title = "Precisión", x = "", y = "") +
   ylim(0, 1)
 
 g4 <- ggplot(subset(data_long, variable == "P_Balanceada"), aes(x = Modelo, y = value, fill = Modelo)) +
   geom_bar(stat = "identity", position = "dodge") +
   scale_fill_brewer(palette = "Purples") +
   theme_minimal() +
-  labs(x = "", y = "", title = "Precisión Balanceada") +
-  theme(axis.text.x = element_blank(), legend.position = "none", axis.title.y = element_blank(), plot.title = element_text(hjust = 0.5, size = 20), axis.text.y = element_blank()) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 18),
+        legend.position = "none",
+        plot.title = element_text(hjust = 0.5, size = 20)) +
+  labs(title = "Prec. Balanceada", x = "", y = "") +
   ylim(0, 1)
 
 g5 <- ggplot(subset(data_long, variable == "Kappa_Coef"), aes(x = Modelo, y = value, fill = Modelo)) +
   geom_bar(stat = "identity", position = "dodge") +
   scale_fill_brewer(palette = "Reds") +
   theme_minimal() +
-  labs(x = "", y = "", title = "Coeficiente Kappa") +
-  theme(axis.text.x = element_blank(), legend.position = "none", axis.title.y = element_blank(), plot.title = element_text(hjust = 0.5, size = 20), axis.text.y = element_blank()) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 18),
+        legend.position = "none",
+        plot.title = element_text(hjust = 0.5, size = 20)) +
+  labs(title = "Kappa", x = "", y = "") +
   ylim(0, 1)
 
 # Combinar los gráficos en una sola fila
